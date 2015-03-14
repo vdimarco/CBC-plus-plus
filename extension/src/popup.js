59b5d9f49c0dcacbfa17d95aa684d56fa133fa9e
vdimarco <vaughn.dimarco@gmail.com>
2015-03-14 19:08:56 -0400

    added widgets folder

--- extension/src/popup.js
+++ extension/src/popup.js
@@ -4,3 +4,17 @@ $(function() {
     console.log(response.example);
   });
 });
+
+
+$(document).ready(function() {
+        fetch_feed();
+});
+
+
+function fetch_feed() {
+        chrome.extension.sendRequest({'action' : 'fetch_feed', 'url' : 'http://lifehacker.com/index.xml'},
+                function(response) {
+                        display_stories(response);
+                }
+        );
+}
